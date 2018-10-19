0df6c1984c3a8f23e932ba57aebfb0085f483f10
martinKindall <martin@rflex.cl>
2018-10-19 20:16:36 -0300

    Se arreglan los tests de Villager

--- test/test/DieTest.java
+++ test/test/DieTest.java
@@ -1,4 +1,12 @@
 package test;
 
 public class DieTest {
+//    @Test
+//    public void cannotRepairDestroyed(){
+//        archer.attack(oldHouse);
+//        assertTrue(!oldHouse.isAlive());
+//
+//        villager.attack(oldHouse);
+//        assertTrue(!oldHouse.isAlive());
+//    }
 }
--- test/test/VillagerTest.java
+++ test/test/VillagerTest.java
@@ -6,54 +6,60 @@ import tarea1.*;
 import static org.junit.Assert.*;
 
 public class VillagerTest {
-    private Attackable barracks;
-    private Attackable oldHouse;
-    private Attacker villager;
+    private Attacker infantry;
+    private Attacker castle;
+    private Attacker villager, villagerEnemy;
+    private Attacker monk;
+    private Attacker siege;
     private Attacker archer;
-    private Monk monk;
+    private Attacker cavalry;
 
 
     @Before
     public void setUp(){
-        barracks = new Barracks(10000000);
-        oldHouse = new Barracks(1);
-        archer = new ArcherUnit(50, 10);
+        infantry = new InfantryUnit(50, 10);
+        castle = new Castle(50, 10);
         villager = new Villager(50, 10);
         monk = new Monk(50, 10);
+        siege = new SiegeUnit(100, 10);
+        archer = new ArcherUnit(50, 10);
+        villagerEnemy = new Villager(50, 10);
+        cavalry = new CavalryUnit(80, 10);
     }
 
 
     @Test
-    public void repairBarracks(){
-        archer.attack(barracks);
-        int currentLife = barracks.currentLife();
-        villager.attack(barracks);
-        assertTrue(currentLife + (int)(villager.attackPts() * 0.7) == barracks.currentLife());
+    public void receiveDmgFromInfantry(){
+        assertTrue(TestUtils.receiveDamageFrom(villager, infantry, 1.5));
     }
 
-
     @Test
-    public void attackArcher(){
-        int currentLife = archer.currentLife();
-        villager.attack(archer);
-        assertTrue(currentLife - (int)(villager.attackPts() * 1.0) == archer.currentLife());
+    public void receiveDmgFromArcher(){
+        assertTrue(TestUtils.receiveDamageFrom(villager, archer, 1.5));
     }
 
-
     @Test
-    public void dontHarmMonk(){
-        int currentLife = monk.currentLife();
-        villager.attack(monk);
-        assertTrue(currentLife == monk.currentLife());
+    public void receiveDmgFromCavalry(){
+        assertTrue(TestUtils.receiveDamageFrom(villager, cavalry, 1.5));
     }
 
+    @Test
+    public void receiveDmgFromSiege(){
+        assertTrue(TestUtils.receiveDamageFrom(villager, siege, 1.5));
+    }
 
     @Test
-    public void cannotRepairDestroyed(){
-        archer.attack(oldHouse);
-        assertTrue(!oldHouse.isAlive());
-        
-        villager.attack(oldHouse);
-        assertTrue(!oldHouse.isAlive());
+    public void receiveDmgFromVillager(){
+        assertTrue(TestUtils.receiveDamageFrom(villager, villagerEnemy, 1.0));
+    }
+
+    @Test
+    public void healedByMonk(){
+        assertTrue(TestUtils.healedRepairedBy(monk, infantry, villager, 0.5));
+    }
+
+    @Test
+    public void receiveDmgFromCastle(){
+        assertTrue(TestUtils.receiveDamageFrom(villager, castle, 1.2));
     }
 }
